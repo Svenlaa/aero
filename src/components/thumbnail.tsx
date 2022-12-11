e2b93f267f9d2c4fefdbf2c8947c1964a137e962
Svenlaa <sven.lammertink@hotmail.com>
2022-12-11 17:49:38 +0100

    Add Featured Video

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -2,31 +2,35 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Thumbnail = ({
-  video
+  video,
+  featured
 }: {
   video: {
     id: string
     title: string
     thumbnailUrl: string
   }
-}) => {
-  return (
-    <Link href={'/v/' + video.id}>
-      <a className="px-2">
-        <div className="relative aspect-video w-full">
-          <Image
-            src={`${process.env.NEXT_PUBLIC_FILE_URL}/${video.thumbnailUrl}`}
-            layout="fill"
-            className="rounded-t-md"
-            alt={`Video thumbnail for ${video.title}`}
-          />
-        </div>
-        <span className="block rounded-b-md bg-slate-400 py-2 px-4">
-          {video.title}
-        </span>
-      </a>
-    </Link>
-  )
-}
+  featured?: true
+}) => (
+  <Link href={'/v/' + video.id}>
+    <a className="px-2">
+      <div className="relative aspect-video w-full">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_FILE_URL}/${video.thumbnailUrl}`}
+          layout="fill"
+          className="rounded-t-md"
+          alt={`Video thumbnail for ${video.title}`}
+        />
+      </div>
+      <span
+        className={`block rounded-b-md py-2 px-4 ${
+          featured ? 'bg-amber-400' : 'bg-slate-400'
+        }`}
+      >
+        {video.title}
+      </span>
+    </a>
+  </Link>
+)
 
 export default Thumbnail
--- src/pages/v.tsx
+++ src/pages/v.tsx
@@ -1,3 +1,4 @@
+import { Video } from '@prisma/client'
 import { createSSGHelpers } from '@trpc/react/ssg'
 import { GetStaticProps } from 'next'
 import Header from '../components/header'
@@ -10,14 +11,22 @@ const Home = () => {
   const videosQuery = trpc.useQuery(['video.getAll', 96])
   if (!videosQuery.isSuccess) return <p>Waiting...</p>
   const videos = videosQuery.data
+  const featuredVideoId = Math.floor(videosQuery.data.length * Math.random())
   return (
     <>
       <Header title="Videos | Aero" />
       <main>
         <div className="mx-2 grid grid-cols-1 gap-4 px-2 pb-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {videos.map((video) => (
-            <Thumbnail video={video} key={video.id} />
-          ))}
+          <Thumbnail
+            video={videos[featuredVideoId]!}
+            key={videos[featuredVideoId]!.id}
+            featured
+          />
+          {videos.map((video, i) =>
+            i === featuredVideoId ? null : (
+              <Thumbnail video={video} key={video.id} />
+            )
+          )}
         </div>
       </main>
     </>
